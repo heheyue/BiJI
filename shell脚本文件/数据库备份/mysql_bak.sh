5f7da99b5411ccf9ccce439e4fb547b7b3e34a89
heheyue <572600301@qq.com>
2021-02-04 11:11:20 +0800

    Update mysql_bak.sh

--- shell脚本文件/数据库备份/mysql_bak.sh
+++ shell脚本文件/数据库备份/mysql_bak.sh
@@ -6,7 +6,7 @@ PORT=3306
 USER=root
 PASSWORD='password'
 #邮件接收人
-Receiver='team_cloud_service@syberos.com 572600301@qq.com'
+Receiver='572600301@qq.com'
 #设置备份文件存储位置
 BDSAVE_PATH='/DB_BackUp'
 function seadmail(){
@@ -25,23 +25,23 @@ function seadmail(){
 time=`date +%Y%m%d-%H%M%S`
 runtime=`echo $time`
 mkdir ${BDSAVE_PATH}/$runtime
-/usr/bin/mysqldump -h$SERVER -P$PORT -u$USER -p$PASSWORD --opt -R syberos_db > ${BDSAVE_PATH}/$runtime/syberos_db_$runtime.sql 
-/usr/bin/mysqldump -h$SERVER -P$PORT -u$USER -p$PASSWORD --opt -R syberos_oadb > ${BDSAVE_PATH}/$runtime/syberos_oadb_$runtime.sql
+/usr/bin/mysqldump -h$SERVER -P$PORT -u$USER -p$PASSWORD --opt -R 
+/usr/bin/mysqldump -h$SERVER -P$PORT -u$USER -p$PASSWORD --opt -R 
 if [[ ! -s ${BDSAVE_PATH}/$runtime/syberos_db_$runtime.sql ]]
 then
         #echo 'sql_bak ERROR'
-	#echo "$runtime 新疆syberos_db库备份失败" | mail -s '新疆数据库备份通知' yanxinyue@syberos.com
-	seadmail '元心数据库备份通知' "$runtime 元心syberos_db库备份失败"
+	#echo "$runtime " | mail -s '' 
+	seadmail 
         exit
 fi
-#/usr/bin/mysqldump -h$SERVER -P$PORT -u$USER -p$PASSWORD --opt -R syberos_oadb > ${BDSAVE_PATH}/$runtime/syberos_oadb_$runtime.sql
+#/usr/bin/mysqldump -h$SERVER -P$PORT -u$USER -p$PASSWORD --opt -R 
 if [[ ! -s ${BDSAVE_PATH}/$runtime/syberos_oadb_$runtime.sql ]]
 then
         #echo 'sql_bak ERROR'
-	#echo "$runtime 新疆syberos_oadb库备份失败" | mail -s '新疆数据库备份通知' yanxinyue@syberos.com
-	seadmail '元心数据库备份通知' "$runtime 元心syberos_oadb库备份失败"
+	#echo "$runtime
+	seadmail 
         exit
 fi
-seadmail '元心数据库备份通知' "$runtime 元心数据库备份成功from160_101server"
-#echo "$runtime 新疆数据库备份成功" | mail -s '新疆数据库备份通知' yanxinyue@syberos.com
+seadmail 
+#echo 
 #echo "sql _bak ok"
